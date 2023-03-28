3ab74f5dd1d7035bf809084be88c3396fc9fe2ea
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-03-28 16:26:30 -0500

    Added event handler for after part selection, but bugs

--- src/components/pew-part/PewPart.jsx
+++ src/components/pew-part/PewPart.jsx
@@ -3,21 +3,25 @@ import React, { useState } from 'react';
 const PewPartSelector = ({ onPartSelected }) => {
     const [selectedPart, setSelectedPart] = useState('');
     const [selectedSubPart, setSelectedSubPart] = useState('');
+    const [submitSelection, setSubmitSelection] = useState(false);
 
-    // Need event handler to handle when a part changes
+    // Event handler to handle when a part changes
     const handlePartChange = (event) => {
         setSelectedPart(event.target.value);
         setSelectedSubPart('');
     }
 
-    // Need event handler for sub types
+    // Event handler for sub types
     const handleSubPartChange = (event) => {
         setSelectedSubPart(event.target.value);
         onPartSelected(selectedPart, event.target.value); // Notifying the parent component of the state change
     }
 
     // Need event handler for submission when making a selection
-    // const handleSubmit
+    const handleSubmitSelection = () => {
+        setSubmitSelection(true);
+        onPartSelected(selectedPart, selectedSubPart);
+    }
 
     // const partTypes = ['Upper-Recievers', 'Lower-Recievers', 'Barrels', 'Triggers', 'Stocks', 'Charging-Handles', 'Optics', 'Bolt-Carrier-Groups', 'Pitol-Grips'];
     const parts = {
@@ -36,36 +40,42 @@ const PewPartSelector = ({ onPartSelected }) => {
         <div className='body'>
             <h1>Build a Pew</h1>
             <div>
-                <label htmlFor='part'>Part: </label>
-                <select id='part' value={selectedPart} onChange={handlePartChange}>
+                {submitSelection ? (
+                    <div>
+                        <strong>
+                            Selected Part: {selectedPart}, Sub-Part: {selectedSubPart}
+                        </strong>
+                    </div>
+                ) : (
+                    <>
+                        <label htmlFor='part'>Part: </label>
+                        <select id='part' value={selectedPart} onChange={handlePartChange}>
 
-                    <option value=''>Select A Part</option>
-                    {Object.keys(parts).map((part) => (
-                        <option key={part} value={part}>
-                            {part}
-                        </option>
-                    ))}
-
-                </select>
+                            <option value=''>Select A Part</option>
+                            {Object.keys(parts).map((part) => (
+                                <option key={part} value={part}>
+                                    {part}
+                                </option>
+                            ))}
+                        </select>
 
                 {selectedPart && (
                     <>
-
                         <label htmlFor='subPart'> Sub-Part: </label>
                         <select id='subPart' value={selectedSubPart} onChange={handleSubPartChange}>
-
                             <option value=''>Select a sub-part</option>
                             {parts[selectedPart].map((subPart) => (
                                 <option key={subPart} value={subPart}>
                                     {subPart}
                                 </option>
                             ))}
-
                         </select>
-
                     </>
                 )}
-
+                {selectedPart && selectedSubPart && (
+                    <button onClick={handleSubmitSelection}>Submit</button>
+                )}
+                </>
             </div>
         </div>
     );
