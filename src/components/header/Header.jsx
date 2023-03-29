9d43263d977322cdfe6ff3b27dd3aa4cc2039016
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-03-29 12:13:48 -0500

    Header component styled within the JSX, for now

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 
 const Header = () => {
-return (
-    <div>
-        <h1>Header</h1>
-    </div>
-)};
+    const headerStyle = {
+        width: '100%',
+        textAlign: 'center',
+        margin: 0
+    };
+
+    const imgStyle = {
+        width: '85%',
+    };
+    return (
+        <div>
+            <header style={headerStyle}>
+                <img src='src/assets/pew-professional.png' alt="Pew Professional" style={imgStyle} />
+            </header>
+        </div>
+    )
+};
 
 export default Header;
