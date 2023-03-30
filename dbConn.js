e4af3415170db013cf158956a2d2c1323372bf1d
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-03-30 17:13:34 -0500

    Added docker, pg, and express server setup

---
+++ dbConn.js
@@ -0,0 +1,39 @@
+// Create a new Pool connection to pg
+
+import pkg from 'pg';
+const { Pool } = pkg;
+// Environmental variables for DB connection or use default if not defined
+// Local environment may not have them, but Render will when deployed
+const POSTGRES_HOST = process.env.POSTGRES_HOST || '127.0.0.1';
+const POSTGRES_DB = process.env.POSTGRES_DB || 'drinkFinder_render';
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || 'password';
+const POSTGRES_USER = process.env.POSTGRES_USER || 'postgres';
+const DATABASE_URL = process.env.DATABASE_URL;
+
+export function getPool() {
+// Object with connection values to pass to a new Pool() to connect to DB
+const dbConfig = {
+    user: POSTGRES_USER,
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    password: POSTGRES_PASSWORD,
+    port: 5432,
+};
+
+// If DATABASE_URL is set as an environmental variable (from Render), use that
+
+let pool = null;
+  if (DATABASE_URL){
+    pool = new Pool({
+          connectionString: process.env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized: false
+          }
+        });
+        
+  } else {
+    pool = new Pool(dbConfig);
+  }
+  return pool;
+};
+export default { getPool };
---
+++ restfulPewServer.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import dotenv from 'dotenv/config';
+import cors from 'cors';
+import express, { json } from 'express';
+const app = express();
+import next from 'process';
+const port = process.env.port || 8000;
+import { getPool } from './dbConn.js';
+import pkg from 'jquery';
+const { data } = pkg;
+const pool = getPool();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS'); // include OPTIONS method
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200); // respond OK to OPTIONS requests
+    }
+    next(); 
+});
+
+app.use(json());
+
+// app.get(all)
+
+// app.get(one, by ID)
+
+// app.post
+
+// app.delete
+
+app.use(cors({ origin: '*' }));
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+    // console.log('Connecting to postgres pool: ', pool);
+});
